78d8868dccca3106c1720026ca21e5794c397147
Clare Pinder <ec2-user@ip-172-31-15-208.eu-west-1.compute.internal>
2019-01-28 16:37:09 +0000

    Step 8, #4 puts with while loop

--- directory.rb
+++ directory.rb
@@ -18,10 +18,10 @@ def print_header
 end
 
 def print(students)
-  students.each_with_index do |student, index|
-    if student[:name].length < 12
-      puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
-    end 
+  entry = 0
+  while entry < students.length do
+    puts "#{students[entry][:name]} (#{students[entry][:cohort]} cohort)"
+    entry += 1
   end
 end
 
