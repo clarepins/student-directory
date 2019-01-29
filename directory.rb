72dff9821a2118c0d363eeff66e3be749cfd4517
Clare Pinder <ec2-user@ip-172-31-15-208.eu-west-1.compute.internal>
2019-01-29 16:54:55 +0000

    Step 10 refactoring

--- directory.rb
+++ directory.rb
@@ -1,30 +1,28 @@
+@students = []
+
 def input_students
-    puts "please enter the names of students and their cohort"
-    puts "press enter twice to end"
-    
-    students = []
-   
-    while true do
-        puts "name?"
-        $name = gets.chomp
-        if !$name.empty?
-          puts "cohort?"
-          $cohort = gets.chomp
-        end 
-      
-      if $name.empty?
-        break
-      end 
-      
-      students << {name: $name, cohort: $cohort}
-      if students.count == 1
-        puts "now we have 1 student"
-      else
-        puts "now we have #{students.count} students"
-      end 
-      
+  puts "please enter the names of students and their cohort"
+  puts "press enter twice to end"
+  
+  while true do
+    puts "name?"
+    name = gets.chomp
+    if !name.empty?
+      puts "cohort?"
+      $cohort = gets.chomp
     end 
-    students
+    
+    if name.empty?
+      break
+    end 
+    
+    @students << {name: name, cohort: $cohort}
+    if @students.count == 1
+      puts "now we have 1 student"
+    else
+      puts "now we have #{@students.count} students"
+    end 
+  end 
 end 
 
 def print_header
@@ -32,11 +30,11 @@ def print_header
   puts "-------------"
 end
 
-def print_by_cohort(students)
+def print_students_by_cohort
   
   cohorts = {}
   
-  students.each do |student|
+  @students.each do |student|
     cohort = student[:cohort]
     if cohorts[cohort] == nil
       cohorts[cohort] = []
@@ -52,41 +50,48 @@ def print_by_cohort(students)
   end 
 end 
 
-def print_footer(names)
-  if names.count == 1
+def print_footer
+  if @students.count == 1
     puts "Overall, we have 1 great student"
   else 
-    puts "Overall, we have #{names.count} great students"
+    puts "Overall, we have #{@students.count} great students"
   end 
 end
 
+def print_menu
+  puts "Menu (select option number)"
+  puts "1. input student(s)"
+  puts "2. list student(s)"
+  puts "9. exit"
+end
+
+def list_students
+  if @students.empty?
+    puts "There are no students at Makers"
+  else 
+    print_header
+    print_students_by_cohort
+    print_footer
+  end 
+end 
+
+def do_menu_choice(menu_choice)
+  case menu_choice 
+    when "1"
+      input_students
+    when "2"
+      list_students
+    when "9"
+      # exit
+    else 
+      puts "Try again"
+  end 
+end 
+
 def interactive_menu
-  students = []
   loop do 
-  # 1 print the menu and ask the user what to do 
-    puts "Menu (select option number)"
-    puts "1. input student(s)"
-    puts "2. list student(s)"
-    puts "9. exit"
-  # 2 read the input and save it to a var
-    menu_choice = gets.chomp
-  # 3 do what the user has asked
-    case menu_choice 
-      when "1"
-        students = input_students
-      when "2"
-        if students.empty?
-          puts "There are no students at Makers"
-        else 
-          print_header
-          print_by_cohort(students)
-          print_footer(students)
-        end 
-      when "9"
-        # exit
-      else 
-        puts "Try again"
-    end 
+    print_menu
+    do_menu_choice(gets.chomp)
   end 
 end 
 
