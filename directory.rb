c262110f183aaaaded99d97034f6333a8d2ea2a4
Clare Pinder <ec2-user@ip-172-31-15-208.eu-west-1.compute.internal>
2019-01-31 11:03:54 +0000

    Step 14 #1 refactor method for input_to_hash

--- directory.rb
+++ directory.rb
@@ -32,6 +32,10 @@ def process(selection)
   end
 end
 
+def input_to_hash(name, cohort = "september")
+  @students << {name: name, cohort: cohort.to_sym}
+end
+
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -40,7 +44,7 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    @students << {name: name, cohort: :november}
+    input_to_hash(name)
     puts "Now we have #{@students.count} students"
     # get another name from the user
     name = STDIN.gets.chomp
@@ -84,7 +88,7 @@ def load_students(filename = "students.csv")
   file = File.open(filename, "r")
   file.readlines.each do |line|
     name, cohort = line.chomp.split(",")
-    @students << {name: name, cohort: cohort.to_sym}
+    input_to_hash(name, cohort)
   end 
   file.close
 end 
